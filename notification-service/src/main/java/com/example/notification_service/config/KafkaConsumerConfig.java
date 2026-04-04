d2621c53c69fd018399d03458e2d08b9f57b555d
parth <parthlakhani112001@gmail.com>
2026-04-04 12:08:29 +0530

    Changes in configuration of notification-service

--- notification-service/src/main/java/com/example/notification_service/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/com/example/notification_service/config/KafkaConsumerConfig.java
@@ -5,39 +5,46 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.example.notification_service.dto.OrderCreatedEvent;
 
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
 
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+	 @Value("${spring.kafka.bootstrap-servers}")
+	 private String bootstrapServers;
+	 
+	 @Bean
+	    public ConsumerFactory<String, OrderCreatedEvent> consumerFactory() {
+	        JsonDeserializer<OrderCreatedEvent> deserializer =
+	                new JsonDeserializer<>(OrderCreatedEvent.class);
+	        deserializer.addTrustedPackages("*");
 
-        Map<String, Object> config = new HashMap<>();
+	        Map<String, Object> config = new HashMap<>();
+	        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // ← uses yml value
+	        config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
+	        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+	        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+	        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+	        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
+	    }
 
-        return new DefaultKafkaConsumerFactory<>(config);
-    }
+	    @Bean
+	    public ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> kafkaListenerContainerFactory() {
+	        ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> factory =
+	                new ConcurrentKafkaListenerContainerFactory<>();
+	        factory.setConsumerFactory(consumerFactory());
+	        return factory;
+	    }
+	}
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-
-        factory.setConsumerFactory(consumerFactory());
-
-        return factory;
-    }
-}
